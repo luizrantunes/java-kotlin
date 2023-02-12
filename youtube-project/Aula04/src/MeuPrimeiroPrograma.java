bebb1f708c7c0525ac33c972b3c88559e0424acd
Luiz Antunes <luizrantunes@outlook.com>
2023-02-11 22:58:44 -0300

    final commit

--- youtube-project/Aula04/src/MeuPrimeiroPrograma.java
+++ youtube-project/Aula04/src/MeuPrimeiroPrograma.java
@@ -1,7 +1,17 @@
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
-        System.out.println("Ola mundo, eu sou o primeiro programa do Luiz em Java");
+        minhaIntrucaoDeImprimir();
     }
+
+    static void minhaIntrucaoDeImprimir() {
+        System.out.println("Ola mundo! Eu sou o primeiro programa do Luiz em Java");
+    }
+
+    // Comentários - São úteis para indicar comentários ou notas nos projetos
+    // Ponto de entrada/Entry point - É o ponto onde todos sistemas procuram
+    // no seu projeto para começar a iniciar o projeto
+    // Métodos - São intruções que você define para o programa executar
+    // Propriedades e variáveis - Onde será armazenado os dados do projeto
 
 }
