4d99839a44e8fc8ae72bfb52b8467ac82aba1a03
Luiz Antunes <luizrantunes@outlook.com>
2023-02-12 00:10:32 -0300

    final commit

--- youtube-project/Aula05/src/MeuSegundoPrograma.java
+++ youtube-project/Aula05/src/MeuSegundoPrograma.java
@@ -5,7 +5,20 @@ public class MeuSegundoPrograma {
         var name = "Luiz ";
         var lastname = "Antunes";
 
+        var newCar = "Honda Fit";   // variaveis -> tipo: Textos (Strings)
+        var year = 2004;             // variaveis -> tipo: Numeros (Int)
+        var km = 200.5;             // variaveis -> tipo: Numeros fracionados (Double)
+        var available = true;       // variaveis -> tipo: Booleanas (Verdadeiro | Falso)
+
         System.out.println(initialText);
         System.out.println(phrase + name + lastname);
+
+        System.out.println(newCar);
+        System.out.println(year);
+        System.out.println(km);
+        System.out.println(available);
     }
 }
+
+// Variaveis -> São espaços de memória alocado representado por tipos
+
