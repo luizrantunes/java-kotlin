80bce52eb9357869d71cc51f155243d838899513
Luiz Antunes <luizrantunes@outlook.com>
2023-02-11 22:20:16 -0300

    initial commit

---
+++ youtube-project/Aula03/src/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 9;
+        int num3 = 27;
+        int soma = num1 + num2 + num3;
+        System.out.println(soma);
+    }
+
+}
